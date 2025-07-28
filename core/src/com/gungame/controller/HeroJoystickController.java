1a30536dd3b063c05e531ec4a2f628186579a468
Povvor <Povvor@yandex.ru>
2025-07-28 22:38:58 +0300

    +интерфейс фикс, +добавил бросок гранаты через контроллер

--- core/src/com/gungame/controller/HeroJoystickController.java
+++ core/src/com/gungame/controller/HeroJoystickController.java
@@ -1,7 +1,6 @@
 package com.gungame.controller;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.Camera;
